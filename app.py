ecba54e04a8e4521a24f16d51f5d67e0a52e27ab
Lucas Rocha <129792862+LcsRocha21@users.noreply.github.com>
2025-05-14 22:13:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -42,4 +42,4 @@ st.markdown(
 st.title("Clique no botão abaixo")
 
 if st.button("Mostrar Stitch"):
-    st.image(r"C:\Users\lucas\OneDrive\Área de Trabalho\Sisters\Imagens\Stitch-2.png", use_container_width=True)
+    st.image("Imagens\Stitch-2.png", use_container_width=True)
